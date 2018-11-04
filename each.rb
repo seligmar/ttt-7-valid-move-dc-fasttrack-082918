10909da5fb7163546e20ba2cc90f2f3ccd54b769
Mary Selig <seligmar@gmail.com>
2018-11-04 18:51:38 +0000

    automatically backed up by learn

--- each.rb
+++ each.rb
@@ -1,5 +1,17 @@
 arr = (1..100).to_a 
 
-def divisible_by_three?(num) 
+def divisible_by?(num, factor) 
+  num % factor
+end 
 
+def fizz_buzz(num, factor)
+  if divisible_by?(num, 3) == 0 && divisible_by?(num, 5) == 0 
+    puts "FizzBuzz"
+    elsif divisible_by?(num, 3) == 0
+    puts "Fizz"
+    elsif divisible_by?(num, 5) == 0
+    puts "Buzz"
+  else 
+    puts num
+  end 
 end 
